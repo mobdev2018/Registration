e0573e29ed6a9ee352f238235210664b82131822
mobdev2018 <mobdev2018@gmail.com>
2018-10-16 13:37:11 -0700

    updated.

--- Registration/ViewController.swift
+++ Registration/ViewController.swift
@@ -7,10 +7,12 @@
 //
 
 import UIKit
+import SkyFloatingLabelTextField
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var contentView: UIView!
+    
     
     
     override func viewDidLoad() {
@@ -19,9 +21,90 @@ class ViewController: UIViewController {
         
         let screenSize = UIScreen.main.bounds.size
         
-        var titleLabel = UILabel(frame: CGRect(x: 30, y: 100, width: screenSize.width, height: 26))
+        let width = screenSize.width - 60
+        
+        let titleLabel = UILabel(frame: CGRect(x: 30, y: 100, width: width, height: 26))
+        titleLabel.font = UIFont(name: "NunitoSans-SemiBold", size: 26)
         titleLabel.text = "Create New Account"
         self.contentView.addSubview(titleLabel)
+        
+        let lightGreyColor = UIColor(red: 197/255.0, green: 197/255.0, blue: 197/255.0, alpha: 1.0)
+        let darkGreyColor = UIColor(red: 52/255.0, green: 52/255.0, blue: 52/255.0, alpha: 1.0)
+        let textFieldFont = UIFont(name: "NunitoSans-Regular", size: 18)
+        let placeholderFont = UIFont(name: "NunitoSans-Regular", size: 14)
+        
+        let firstNameTextField = SkyFloatingLabelTextField(frame: CGRect(x: 30, y: 160, width: width, height: 50))
+        firstNameTextField.placeholder = "First Name"
+        firstNameTextField.title = "First Name"
+        firstNameTextField.textColor = darkGreyColor
+        firstNameTextField.lineColor = lightGreyColor
+        firstNameTextField.selectedTitleColor = lightGreyColor
+        firstNameTextField.font = textFieldFont
+        firstNameTextField.placeholderFont = placeholderFont
+        self.contentView.addSubview(firstNameTextField)
+        
+        let lastNameTextField = SkyFloatingLabelTextField(frame: CGRect(x: 30, y: 230, width: width, height: 50))
+        lastNameTextField.placeholder = "Last Name"
+        lastNameTextField.title = "Last Name"
+        lastNameTextField.textColor = darkGreyColor
+        lastNameTextField.lineColor = lightGreyColor
+        lastNameTextField.selectedTitleColor = lightGreyColor
+        lastNameTextField.font = textFieldFont
+        lastNameTextField.placeholderFont = placeholderFont
+        self.contentView.addSubview(lastNameTextField)
+        
+        let emailTextField = SkyFloatingLabelTextField(frame: CGRect(x: 30, y: 300, width: width, height: 50))
+        emailTextField.placeholder = "Email"
+        emailTextField.title = "Email"
+        emailTextField.textColor = darkGreyColor
+        emailTextField.lineColor = lightGreyColor
+        emailTextField.selectedTitleColor = lightGreyColor
+        emailTextField.font = textFieldFont
+        emailTextField.placeholderFont = placeholderFont
+        self.contentView.addSubview(emailTextField)
+        
+        let phoneTextField = SkyFloatingLabelTextField(frame: CGRect(x: 30, y: 370, width: width, height: 50))
+        phoneTextField.placeholder = "Mobile"
+        phoneTextField.title = "Phone Number"
+        phoneTextField.textColor = darkGreyColor
+        phoneTextField.lineColor = lightGreyColor
+        phoneTextField.selectedTitleColor = lightGreyColor
+        phoneTextField.font = textFieldFont
+        phoneTextField.placeholderFont = placeholderFont
+        self.contentView.addSubview(phoneTextField)
+        
+        let passwordTextField = SkyFloatingLabelTextField(frame: CGRect(x: 30, y: 440, width: width, height: 50))
+        passwordTextField.placeholder = "Password"
+        passwordTextField.title = "Password"
+        passwordTextField.textColor = darkGreyColor
+        passwordTextField.lineColor = lightGreyColor
+        passwordTextField.selectedTitleColor = lightGreyColor
+        passwordTextField.font = textFieldFont
+        passwordTextField.isSecureTextEntry = true
+        passwordTextField.placeholderFont = placeholderFont
+        self.contentView.addSubview(passwordTextField)
+        
+        
+        let descriptionLabel = UILabel(frame: CGRect(x: 30, y: 510, width: width, height: 50))
+        let strDescription = NSMutableAttributedString(string: "By Signing Up, you are agree to FavorCare ", attributes: [NSAttributedStringKey.font: UIFont(name: "NunitoSans-Regular", size: 12)!])
+        let termsString = NSAttributedString(string: "Terms of Service ", attributes: [NSAttributedStringKey.font: UIFont(name: "NunitoSans-Bold", size: 12)!])
+        let andString = NSAttributedString(string: "and", attributes: [NSAttributedStringKey.font: UIFont(name: "NunitoSans-Regular", size: 12)!])
+        let privacyString = NSAttributedString(string: " Privacy Policy", attributes: [NSAttributedStringKey.font: UIFont(name: "NunitoSans-Bold", size: 12)!])
+        strDescription.append(termsString)
+        strDescription.append(andString)
+        strDescription.append(privacyString)
+        descriptionLabel.attributedText = strDescription
+        descriptionLabel.numberOfLines = 0
+        descriptionLabel.textAlignment = .center
+        self.contentView.addSubview(descriptionLabel)
+        
+        let signupButton = UIButton(frame: CGRect(x: 30, y: 570, width: width, height: 50))
+        signupButton.setTitle("Sign Up", for: UIControlState.normal)
+        signupButton.backgroundColor = UIColor(red: 0, green: 132/255.0, blue: 191/255.0, alpha: 1.0)
+        signupButton.layer.cornerRadius = 5
+        self.contentView.addSubview(signupButton)
+        
+        
         
     }
 
