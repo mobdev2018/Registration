00f6f8456eb4815b95ea9a6f2198bc600f601695
mobdev2018 <mobdev2018@gmail.com>
2018-10-16 12:03:20 -0700

    initial commit (SkyFloatingLabelTextField integrated).

--- Registration/ViewController.swift
+++ Registration/ViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var contentView: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let screenSize = UIScreen.main.bounds.size
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
